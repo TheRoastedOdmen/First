d70b28ec6a17a61c716125a9804882760fd9415b
TheRoastedOdmen <52803452+TheRoastedOdmen@users.noreply.github.com>
2019-07-13 09:53:25 +0300

    Add files via upload

---
+++ Simple calcuator.py
@@ -0,0 +1,24 @@
+print('\n                                    ########################################')
+print('                                    ### S I M P L E  C A L C U L A T O R ###')
+print('                                    ########################################\n')
+x = float(input ('Enter x: '))
+print(x,'\n#########')
+y = float(input ('Enter y: '))
+print(y,'\n#########')
+
+print ('\nAddition:', x + y)
+
+print ('\nSubstraction:', x - y)
+
+print ('\nMultiplication:', x*y)
+
+print ('\nDivision:', x/y)
+
+print ('(Persentage):', (x/y)*100, '%')
+
+x = int(x)
+y = int(y)
+
+print ('\n\nBinary int(x):', bin(x)[2:])
+
+print ('\nBinary int(y):', bin(y)[2:])
