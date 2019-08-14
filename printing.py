5c3541eef4d2e99d8d5302195cf7ea760c0b2072
TheRoastedOdmen <v.i.selivantsev@gmail.com>
2019-08-15 00:32:48 +0300

    Learning

---
+++ printing.py
@@ -0,0 +1,4 @@
+print('\nFirstly choose an option\n'
+    'Then input the numbers via enter\n'
+    'When you done inputting press enter to get the result\n'
+    'Note that yopu can input any amount of numbers\n')
