b4db09f0ba1bd6477fc6ddaea5d06a2fc479e218
TheRoastedOdmen <v.i.selivantsev@gmail.com>
2019-08-19 14:27:05 +0300

    Delete printing.py

--- printing.py
+++
@@ -1,4 +0,0 @@
-print('\nFirstly choose an option\n'
-    'Then input the numbers via enter\n'
-    'When you done inputting press enter to get the result\n'
-    'Note that yopu can input any amount of numbers\n')
