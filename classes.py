9eba1482e57582ed33aed04e38bf6d8e8981bbdf
TheRoastedOdmen <v.i.selivantsev@gmail.com>
2019-09-19 13:29:01 +0300

    Learning

---
+++ classes.py
@@ -0,0 +1,21 @@
+class Student: 
+    def __init__(self, name):
+        self.name = name
+        
+    def sayhi(self):
+        print('Hi from ' + self.name)
+
+s1 = Student('Dima')
+s1.sayhi()
+
+class Dog:
+    def __init__(self, name, color, bark):
+        self.name = name
+        self.color = color
+        self.bark = bark
+
+    #def bark(self):
+    #    print("Woof!") #not needed cuz 15 line
+
+fido = Dog("Fido", "brown","Woof!")
+print(fido.name,fido.bark)
