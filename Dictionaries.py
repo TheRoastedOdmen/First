adefb1cfcc0293f1f78a216cf7edb283230f8038
TheRoastedOdmen <v.i.selivantsev@gmail.com>
2019-10-08 08:23:55 +0300

    Learning

--- Dictionaries.py
+++ Dictionaries.py
@@ -1,3 +1,9 @@
 values = {'a':0.1,'b':0.25,'c':0.33, 'd':0.5, 'f':0.75}
 
 print(str((((values['a']+values['b']+values['c']+values['d']+values['f'])/len(values))*100)) + ' %')
+
+perc = {'x':1, 'y':2, 'z':3}
+
+for i in perc:
+    print(i)
+ 
