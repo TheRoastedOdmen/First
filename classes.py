6f17bbab219e5893b0f8d468607ae2febfb78f02
TheRoastedOdmen <v.i.selivantsev@gmail.com>
2019-10-20 12:51:15 +0300

    l

--- classes.py
+++ classes.py
@@ -10,21 +10,22 @@ print()
 s1.sayhi()
 
 class Animal:
-    def __init__(self,name,color):
-        self.name=name
-        self.color=color
-class cat(Animal):
-    def purr(self):
-        print("Purr")
-class dog(Animal):
-    def bark(self):
-        print("Woof")
+    def __init__(self,name,color,voice):
+        self.name = name
+        self.color = color
+        self.voice = voice
+#class cat(Animal):
+    #def purr(self):
+       # print("Purr")
+#class dog(Animal):
+    #def bark(self):
+       # print("Woof")
 
-fido = dog("Fido", "brown")
-Teffi = cat("Teffi", "white")
+fido = Animal("Fido", "brown", "bark")
+Teffi = Animal("Teffi", "white", "purr")
 print()
-print(fido.name,fido.color)
-fido.bark()
+print(fido.name, fido.color, fido.voice)
+#fido.bark()
 print()
-print(Teffi.name,Teffi.color)
-Teffi.purr()
+print(Teffi.name, Teffi.color, Teffi.voice)
+#Teffi.purr()
