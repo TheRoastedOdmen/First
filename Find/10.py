09773bedda074bf9708d796d6f770f7dc90e8f71
TheRoastedOdmen <v.i.selivantsev@gmail.com>
2019-11-15 13:44:48 +0300

    l

--- Find/10.py
+++ Find/10.py
@@ -5,3 +5,11 @@ for i in range(N):
     flag  = (x % 10 == 0) or flag
 print(flag)
 #atleast 1 number must be %10 for True
+
+flag = True
+N = int(input('N: '))
+for i in range(N):
+    x = int(input('x: '))
+    flag = flag and x % 10 == 0
+print(flag)
+#all numbers must be %10 for True
