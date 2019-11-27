784541f3009780e87de14af34b56e75b6a937444
TheRoastedOdmen <v.i.selivantsev@gmail.com>
2019-11-27 03:01:33 +0300

    l

---
+++ checking.py
@@ -0,0 +1,12 @@
+base = 7
+x = int(input())
+while x > 0:
+    print()
+    dig = x % base
+    print(dig)
+    #print(dig, end='') #no line break
+    print()
+    x //= base
+    print(x)
+    
+print('end')
