4ef9873cf11197c5c835dea8169b4d56c81e18f5
TheRoastedOdmen <v.i.selivantsev@gmail.com>
2019-12-10 10:01:22 +0300

    Random generaotr of even numbers

---
+++ Random generator/rg_even.py
@@ -0,0 +1,20 @@
+from random import randint
+from time import sleep
+
+amount = int(input("Quantity of numbers: "))
+range1 = int(input("From: "))
+range2 = int(input("To: "))
+
+generated_list = []
+
+print('Let the generation commence ^ . ^')
+
+sleep(1)
+
+while amount > 0:
+    random_i = randint(range1, range2)
+    if random_i % 2 == 0:
+        generated_list.append(random_i)
+        amount -= 1
+
+print('Result:',generated_list)
