614806f5705f895bdd9fd1e63eb7b0770a96648a
Vasily <v.i.selivantsev@gmail.com>
2019-12-15 10:59:17 +0300

    l

---
+++ how to make a module/max2min2.py
@@ -0,0 +1,18 @@
+#x = int(input('x: '))
+#y = int(input('y: '))
+
+def max2(x,y):
+    if x > y:
+        return x
+    return y
+
+#print()
+#print(max2('abd','abc'))
+
+def min2(x,y):
+    if x < y:
+        return x
+    return y
+
+#print()
+#print(min2(x,y))  
