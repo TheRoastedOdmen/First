add4a0bccd111412039e5e599b3ce71a80d8c18d
Vasily <v.i.selivantsev@gmail.com>
2020-06-11 13:53:03 +0300

    coursera

---
+++ Coursera/объект none.py
@@ -0,0 +1,16 @@
+income = 0
+income2 = None
+
+print()
+
+if income is None:
+    print("Еще не начинали продавать")
+elif not income:
+    print("Ничего не заработали")
+
+print()
+
+if income2 is None:
+    print("Еще не начинали продавать")
+elif not income2:
+    print("Ничего не заработали")
