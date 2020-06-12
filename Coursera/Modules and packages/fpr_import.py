598eecb344247061f24c02087135234f8068e0ff
Vasily <v.i.selivantsev@gmail.com>
2020-06-12 16:48:02 +0300

    Coursera Task

---
+++ Coursera/Modules and packages/fpr_import.py
@@ -0,0 +1,5 @@
+from vars import *
+
+# print(vars.x + vars.y)
+print(multi(4, 5))  # note that vars.multi won't work (instead needed to import vars, but otherwise __all__ attribute won't work)
+# print(vars.div(4, 5))
