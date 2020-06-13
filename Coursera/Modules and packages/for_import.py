bd99bb821aaf2ba1110ffb306fe0a3db5831a158
Vasily <v.i.selivantsev@gmail.com>
2020-06-13 16:09:59 +0300

    coursera

--- Coursera/Modules and packages/for_import.py
+++ Coursera/Modules and packages/for_import.py
@@ -2,4 +2,4 @@ from vars import *
 
 # print(vars.x + vars.y)
 print(multi(4, 5))  # note that vars.multi won't work (instead needed to import vars, but otherwise __all__ attribute won't work)
-# print(vars.div(4, 5))
+# print(div(4, 5))  # to being able to work add the div in __all__ in vars module
