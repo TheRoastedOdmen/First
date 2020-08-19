a39e2940b7ccad3d9cb3647429d739e48be44da4
Vasily <v.i.selivantsev@gmail.com>
2020-08-19 03:40:17 +0300

    c

--- Coursera/classes/class dict.py
+++ Coursera/classes/class dict.py
@@ -1,15 +1,17 @@
 class Planet:
 	"""This class describes planets"""
 
-	count = 1  # аттрибут класса
+	count = 0  # аттрибут класса
 
 	def __init__(self, name, population=None):  # переопределен метод инит
 		self.name = name  # аттрибут метода
 		self.population = population or []  # аттрибут метода
+		Planet.count += 1
 
 
-planet = Planet("Earth")
-
+planet = Planet("Earth", 8)
+planet.population = 9
+print(Planet.count)
 print(planet.__dict__)
 
 planet.mass = 5.97e24
